fea67cca5ffeeb347f5be97a1dd1358b3dfdef03
Henning Liebenau <henning.liebenau@protonmail.com>
2023-05-15 20:48:23 +0200

    WiC64: Added some temporary code to test transfers

--- wic64/wic64.cpp
+++ wic64/wic64.cpp
@@ -10,6 +10,21 @@ Display *display;
 Connection *connection;
 Userport *userport;
 
+#define REQUEST_SIZE 0x27
+#define RESPONSE_SIZE 0x02 + 0x04
+
+uint8_t request[REQUEST_SIZE];
+uint8_t response[RESPONSE_SIZE] = { 0x00, 0x04, 0xde, 0xad, 0xbe, 0xef };
+
+void responseSent() {
+  log_buf_d(response, (size_t) RESPONSE_SIZE);
+}
+
+void requestReceived() {
+  log_buf_d(request, (size_t) REQUEST_SIZE);
+  userport->send(response, RESPONSE_SIZE, responseSent);
+}
+
 void setup() {
   display    = new Display();
   connection = new Connection(display);
@@ -17,6 +32,10 @@ void setup() {
 }
 
 void loop() {
+  if(userport->isIdle() && userport->isReadyToReceive()) {
+    userport->receive(request, REQUEST_SIZE, requestReceived);
+  }
+
   // needed until we are busy enough to avoid the watchdog
-  vTaskDelay(pdMS_TO_TICKS(1000));
+  vTaskDelay(1);
 }
