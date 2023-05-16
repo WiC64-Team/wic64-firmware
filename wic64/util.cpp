68144890bc64847c16877c1666d1973f66a83994
Henning Liebenau <henning.liebenau@protonmail.com>
2023-05-16 20:50:08 +0200

    Utilities: Improved hexdump() function, added ascii char column
    Improved hexdump() function to print in a more familiar format,
    including an additional column displaying the data as ascii chars.
    
    Also use correct datatypes for variables so that the function can now
    display data buffers above 64kb.

--- wic64/util.cpp
+++ wic64/util.cpp
@@ -1,23 +1,63 @@
 #include <cstdlib>
 #include <cstdio>
 #include <cstring>
+#include <cstdint>
+#include <cctype>
 #include "esp32-hal-log.h"
 
-void hexdump(const char* msg, uint8_t *data, uint16_t size) {
-  char *str = (char*) calloc(strlen(msg)+1 + size * 3 + (size/16), sizeof(uint8_t) + 1);
-  char *pos = str;
+void hexdump(const char* title, uint8_t *data, uint32_t size) {
+  const uint8_t newline = 1;
+  const uint8_t nullbyte = 1;
+  const uint8_t bytes_per_line = 16;
+  const uint8_t chars_per_line = (bytes_per_line * 4) + 10 + 1 + 2;
 
-  snprintf(pos, strlen(msg)+2, "%s\n", msg);
-  pos = pos + strlen(msg) + 1;
+  uint64_t num_lines = (size / bytes_per_line) + ((size % bytes_per_line) ? 1 : 0);
+  uint64_t required = strlen(title) + (2 * newline) + (num_lines * chars_per_line) + nullbyte;
 
-  for (uint8_t i=0; i<size; i++) {
-    snprintf(pos, 5, "%02X ", data[i]);
-    pos += 3;
+  char *str = (char*) calloc(required, sizeof(char));
+  char *line = str;
 
-    if ((i+1) % 16 == 0) {
-      pos[0] = '\n';
-      pos++;
+  line += snprintf(line, strlen(title)+3, "%s\n\n", title);
+
+  uint64_t addr;
+  uint64_t data_index;
+  char* cursor;
+
+  for(uint64_t line_index = 0; line_index < num_lines; line_index++) {
+    cursor = line;
+
+    addr = line_index * bytes_per_line;
+    cursor += snprintf(cursor, 9, "%08llx", addr);
+
+    cursor += snprintf(cursor, 4, "| ");
+
+    for(uint8_t i=0; i<bytes_per_line; i++) {
+      data_index = line_index * bytes_per_line + i;
+
+      if(data_index < size) {
+        cursor += snprintf(cursor, 3, "%02x", data[data_index]);
+        cursor += snprintf(cursor, 2, " ");
+      }
+      else {
+        cursor += snprintf(cursor, 4, "   ");
+      }
     }
+    cursor += snprintf(cursor, 2, "|");
+
+    for(uint8_t i=0; i<bytes_per_line; i++) {
+      data_index = line_index * bytes_per_line + i;
+      if(data_index < size) {
+        uint8_t c = data[data_index];
+        cursor[i] = isprint((char)c) ? c : '.';
+      }
+      else {
+        cursor[i] = ' ';
+      }
+    }
+    cursor += bytes_per_line;
+    cursor += snprintf(cursor, 3, "|\n");
+
+    line += chars_per_line;
   }
 
   log_d("%s", str);
--- wic64/util.h
+++ wic64/util.h
@@ -3,6 +3,6 @@
 
 #include <cstdint>
 
-void hexdump(const char *msg, uint8_t *data, uint16_t size);
+void hexdump(const char *msg, uint8_t *data, uint32_t size);
 
 #endif // WIC64_HEXDUMP_H
