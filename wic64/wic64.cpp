3c9c89c75a66d9685dc9347d2e828876231f8697
Henning Liebenau <henning.liebenau@protonmail.com>
2023-05-19 09:14:42 +0200

    Wic64: Removed test code, corrected indentation

--- wic64/wic64.cpp
+++ wic64/wic64.cpp
@@ -7,30 +7,12 @@ Display *display;
 Connection *connection;
 Userport *userport;
 
-#define REQUEST_SIZE 0x27
-#define RESPONSE_SIZE 0x02 + 0x04
-
-uint8_t request[REQUEST_SIZE];
-uint8_t response[RESPONSE_SIZE] = { 0x00, 0x04, 0xde, 0xad, 0xbe, 0xef };
-
-void responseSent() {
-  hexdump("Response:", response, RESPONSE_SIZE);
-}
-
-void requestReceived() {
-  hexdump("Request:", request, REQUEST_SIZE);
-  userport->send(response, RESPONSE_SIZE, responseSent);
-}
-
 void setup() {
-  display    = new Display();
-  connection = new Connection(display);
-  userport   = new Userport();
+    display    = new Display();
+    connection = new Connection(display);
+    userport   = new Userport();
 }
 
 void loop() {
-  if (userport->isReadyToReceive()) {
-    userport->receive(request, REQUEST_SIZE, requestReceived, Userport::TIMEOUT_NONE);
-  }
-  vTaskDelay(pdMS_TO_TICKS(10));
+    vTaskDelay(pdMS_TO_TICKS(10));
 }
