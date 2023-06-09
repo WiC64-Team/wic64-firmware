c1e5209cec7e643fc6f67e9f47f03b7e95c84c5d
Henning Liebenau <henning.liebenau@protonmail.com>
2023-06-09 15:42:41 +0200

    Display/Utilities: Fixed indentation and formatting

--- wic64/display.cpp
+++ wic64/display.cpp
@@ -3,98 +3,98 @@
 #include "display.h"
 
 Display::Display() {
-  log_d("Initializing I2C interface");
-  if (!Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN)) {
-    log_e("Could not initialize I2C interface");
-    return;
-  }
+    log_d("Initializing I2C interface");
+        if (!Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN)) {
+        log_e("Could not initialize I2C interface");
+        return;
+    }
 
-  log_d("Creating display instance");
-  display = new Adafruit_SSD1306(WIDTH, HEIGHT, &Wire, RESET_PIN_NOT_CONNECTED);
+    log_d("Creating display instance");
+    display = new Adafruit_SSD1306(WIDTH, HEIGHT, &Wire, RESET_PIN_NOT_CONNECTED);
 
-  log_i("Allocating SSD1306 buffer");
-  if (!display->begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    log_e("Could not allocate buffer for SSD1306");
-    display = NULL;
-    return;
-  }
+    log_i("Allocating SSD1306 buffer");
+    if (!display->begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+        log_e("Could not allocate buffer for SSD1306");
+        display = NULL;
+        return;
+    }
 
-  display->setTextColor(WHITE);
-  display->setTextSize(1);
+    display->setTextColor(WHITE);
+    display->setTextSize(1);
 
-  display->clearDisplay();
-  display->drawBitmap(0, 0, logo, WIDTH, HEIGHT, WHITE);
-  display->display();
+    display->clearDisplay();
+    display->drawBitmap(0, 0, logo, WIDTH, HEIGHT, WHITE);
+    display->display();
 }
 
 void Display::setRotation(uint8_t rotation) {
-  this->rotation = rotation;
-  update();
+    this->rotation = rotation;
+    update();
 }
 
 void Display::setIp(String ip) {
-  this->ip = ip;
-  update();
+    this->ip = ip;
+    update();
 }
 
 void Display::resetIp(void) {
-  this->ip = "0.0.0.0";
-  update();
+    this->ip = "0.0.0.0";
+    update();
 }
 
 void Display::setSSID(String ssid) {
-  this->ssid = ssid;
-  update();
+    this->ssid = ssid;
+    update();
 }
 
 void Display::setRSSI(int8_t rssi) {
-  snprintf(this->rssi, MAX_CHARS_FOR_RSSI, "%ddBm", rssi);
-  update();
+    snprintf(this->rssi, MAX_CHARS_FOR_RSSI, "%ddBm", rssi);
+    update();
 }
 
 void Display::setStatus(String status) {
-  this->status = status;
-  update();
+    this->status = status;
+    update();
 }
 
 char *Display::abbreviated(String string) {
-  return abbreviated(string, MAX_CHARS_PER_LINE);
+    return abbreviated(string, MAX_CHARS_PER_LINE);
 }
 
 char *Display::abbreviated(String string, uint8_t width) {
-  if (width > MAX_CHARS_PER_LINE) {
-    width = MAX_CHARS_PER_LINE;
-  }
+    if (width > MAX_CHARS_PER_LINE) {
+        width = MAX_CHARS_PER_LINE;
+    }
 
-  return strncpy(line, string.c_str(), width);
+    return strncpy(line, string.c_str(), width);
 }
 
 void Display::printStatusAndRSSI(void) {
-  uint8_t rssi_width = strlen(rssi);
-  display->print(abbreviated(status, MAX_CHARS_PER_LINE - rssi_width - 1));
+    uint8_t rssi_width = strlen(rssi);
+    display->print(abbreviated(status, MAX_CHARS_PER_LINE - rssi_width - 1));
 
-  uint8_t abbreviated_status_width = strlen(line);
-  uint8_t gap = MAX_CHARS_PER_LINE - abbreviated_status_width - rssi_width;
+    uint8_t abbreviated_status_width = strlen(line);
+    uint8_t gap = MAX_CHARS_PER_LINE - abbreviated_status_width - rssi_width;
 
-  while(gap--) display->print(" ");
-  display->println(rssi);
+    while(gap--) display->print(" ");
+    display->println(rssi);
 }
 
 void Display::update() {
-  if (display == NULL) return;
+    if (display == NULL) return;
 
-  display->clearDisplay();
-  display->setRotation(rotation);
+    display->clearDisplay();
+    display->setRotation(rotation);
 
-  display->setFont(FONT_BIG);
-  display->setCursor(0, 12);
-  display->println(ip);
+    display->setFont(FONT_BIG);
+    display->setCursor(0, 12);
+    display->println(ip);
 
-  display->setFont(FONT_BUILTIN);
-  display->setCursor(0, 20);
-  display->println(abbreviated(ssid));
+    display->setFont(FONT_BUILTIN);
+    display->setCursor(0, 20);
+    display->println(abbreviated(ssid));
 
-  printStatusAndRSSI();
+    printStatusAndRSSI();
 
-  display->display();
+    display->display();
 }
--- wic64/utilities.cpp
+++ wic64/utilities.cpp
@@ -6,60 +6,60 @@
 #include "esp32-hal-log.h"
 
 void hexdump(const char* title, uint8_t *data, uint32_t size) {
-  const uint8_t newline = 1;
-  const uint8_t nullbyte = 1;
-  const uint8_t bytes_per_line = 16;
-  const uint8_t chars_per_line = (bytes_per_line * 4) + 10 + 1 + 2;
+    const uint8_t newline = 1;
+    const uint8_t nullbyte = 1;
+    const uint8_t bytes_per_line = 16;
+    const uint8_t chars_per_line = (bytes_per_line * 4) + 10 + 1 + 2;
 
-  uint64_t num_lines = (size / bytes_per_line) + ((size % bytes_per_line) ? 1 : 0);
-  uint64_t required = strlen(title) + (2 * newline) + (num_lines * chars_per_line) + nullbyte;
+    uint64_t num_lines = (size / bytes_per_line) + ((size % bytes_per_line) ? 1 : 0);
+    uint64_t required = strlen(title) + (2 * newline) + (num_lines * chars_per_line) + nullbyte;
 
-  char *str = (char*) calloc(required, sizeof(char));
-  char *line = str;
+    char *str = (char*) calloc(required, sizeof(char));
+    char *line = str;
 
-  line += snprintf(line, strlen(title)+3, "%s\n\n", title);
+    line += snprintf(line, strlen(title)+3, "%s\n\n", title);
 
-  uint64_t addr;
-  uint64_t data_index;
-  char* cursor;
+    uint64_t addr;
+    uint64_t data_index;
+    char* cursor;
 
-  for(uint64_t line_index = 0; line_index < num_lines; line_index++) {
-    cursor = line;
+    for (uint64_t line_index = 0; line_index < num_lines; line_index++) {
+        cursor = line;
 
-    addr = line_index * bytes_per_line;
-    cursor += snprintf(cursor, 9, "%08llx", addr);
+        addr = line_index * bytes_per_line;
+        cursor += snprintf(cursor, 9, "%08llx", addr);
 
-    cursor += snprintf(cursor, 4, "| ");
+        cursor += snprintf(cursor, 4, "| ");
 
-    for(uint8_t i=0; i<bytes_per_line; i++) {
-      data_index = line_index * bytes_per_line + i;
+        for ( uint8_t i=0; i<bytes_per_line; i++) {
+            data_index = line_index * bytes_per_line + i;
 
-      if(data_index < size) {
-        cursor += snprintf(cursor, 3, "%02x", data[data_index]);
-        cursor += snprintf(cursor, 2, " ");
-      }
-      else {
-        cursor += snprintf(cursor, 4, "   ");
-      }
+            if (data_index < size) {
+                cursor += snprintf(cursor, 3, "%02x", data[data_index]);
+                cursor += snprintf(cursor, 2, " ");
+            }
+            else {
+                cursor += snprintf(cursor, 4, "   ");
+            }
+        }
+        cursor += snprintf(cursor, 2, "|");
+
+        for (uint8_t i=0; i<bytes_per_line; i++) {
+            data_index = line_index * bytes_per_line + i;
+            if (data_index < size) {
+                uint8_t c = data[data_index];
+                cursor[i] = isprint((char)c) ? c : '.';
+            }
+            else {
+                cursor[i] = ' ';
+            }
+        }
+        cursor += bytes_per_line;
+        cursor += snprintf(cursor, 3, "|\n");
+
+        line += chars_per_line;
     }
-    cursor += snprintf(cursor, 2, "|");
 
-    for(uint8_t i=0; i<bytes_per_line; i++) {
-      data_index = line_index * bytes_per_line + i;
-      if(data_index < size) {
-        uint8_t c = data[data_index];
-        cursor[i] = isprint((char)c) ? c : '.';
-      }
-      else {
-        cursor[i] = ' ';
-      }
-    }
-    cursor += bytes_per_line;
-    cursor += snprintf(cursor, 3, "|\n");
-
-    line += chars_per_line;
-  }
-
-  log_d("%s", str);
-  free(str);
+    log_d("%s", str);
+    free(str);
 }
