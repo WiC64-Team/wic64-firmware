85363cba09b0717db57f5d94735e9aaec86a3f09
Henning Liebenau <henning.liebenau@protonmail.com>
2023-11-25 17:42:43 +0100

    Configured: Report the current passphrase length in response payload

--- wic64/commands/configured.cpp
+++ wic64/commands/configured.cpp
@@ -1,11 +1,13 @@
 
 #include "configured.h"
 #include "connection.h"
+#include "settings.h"
 
 namespace WiC64 {
     const char* Configured::TAG = "CONFIGURED";
 
     extern Connection* connection;
+    extern Settings* settings;
 
     const char* Configured::describe() {
         return "Configured (Check if WiFi credentials present)";
@@ -18,6 +20,9 @@ namespace WiC64 {
     void Configured::execute(void) {
         if (!connection->configured()) {
             error(Command::INTERNAL_ERROR, "WiFi not configured");
+        }
+        else {
+            response()->appendByte(settings->passphraseLength());
         }
         responseReady();
     }
